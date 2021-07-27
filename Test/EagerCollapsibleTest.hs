41b74ed7cf03f4128ee9d03768b567c0338c6a8b
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-07-27 16:30:49 -0500

    Rewrote isEC tests to be in Tasty.HUnit format

--- Test/EagerCollapsibleTest.hs
+++ Test/EagerCollapsibleTest.hs
@@ -1,40 +1,111 @@
 module EagerCollapsibleTest(
-eagerCollapsibleTests
+    eagerCollapsibleTests
 ) where
 
-import Test.HUnit
+--import Test.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 import Data.List
 
 import Token.Util.EagerCollapsible as EC
 
+-- | main
+eagerCollapsibleTests = defaultMain tests
+
+-- | All EC tests
+tests = testGroup "Token.Util.EagerCollapsible Tests" testList where
+    testList =
+        [
+            dropInfixTests,
+            isECTests,
+            tBTests
+        ]
+
+
 -- |Unit tests for Token.Util.EagerCollapsible.dropInfix
-dropInfix_normal = TestCase $ assertEqual      "For dropInfix [2,3] [1,2,3,4] return [1,4]"     [1,4]     (dropInfix [2,3] [1,2,3,4])
-dropInfix_not_infix = TestCase $ assertEqual   "For dropInfix [2,4] [1,2,3,4] return [1,2,3,4]" [1,2,3,4] (dropInfix [2,4] [1,2,3,4])
-dropInfix_empty_infix = TestCase $ assertEqual "For dropInfix [] [a] return [a]"                [1,2,3,4] (dropInfix [] [1,2,3,4])
-dropInfix_empty_list = TestCase $ assertEqual  "For dropInfix _ [] return []"                   []        (dropInfix [2,3] [])
-dropInfix_testList = TestList [dropInfix_normal, dropInfix_not_infix, dropInfix_empty_infix, dropInfix_empty_list]
-dropInfixTests = runTestTT dropInfix_testList
+dropInfixTests = testGroup "dropInfix" testList where
+    testList = [
+        dropInfix_normal,
+        dropInfix_not_infix,
+        dropInfix_empty_infix,
+        dropInfix_empty_list
+        ]
+dropInfix_normal = testCase name assertion where
+    name      = "For dropInfix [2,3] [1,2,3,4] return [1,4]"
+    assertion = assertEqual name assert func
+    assert    = [1,4]
+    func      = (dropInfix [2,3] [1,2,3,4])
+dropInfix_not_infix = testCase name assertion where
+    name      = "For dropInfix [2,4] [1,2,3,4] return [1,2,3,4]"
+    assertion = assertEqual name assert func
+    assert    = [1,2,3,4]
+    func      = (dropInfix [2,4] [1,2,3,4])
+dropInfix_empty_infix = testCase name assertion where
+    name      = "For dropInfix [] [a] return [a]"
+    assertion = assertEqual name assert func
+    assert    = [1,2,3,4]
+    func      = (dropInfix [] [1,2,3,4])
+dropInfix_empty_list = testCase name assertion where
+    name      = "For dropInfix _ [] return []"
+    assertion = assertEqual name assert func
+    assert    = []
+    func      = (dropInfix [2,3] [])
+
 
 
 -- |Unit tests for Token.Util.EagerCollapsible.isEagerCollapsible as isEC
-isEC_returns_true_for_EC = TestCase $ assertEqual             "return true for isEagerCollapsible (isPrefixOf \"\"\") (isSuffixOf \"\"\") [\"1\",\"\"Hello\",\" simple\", \" test\"\", \"1\"]"    True  (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") [" simple", " test\"","1"])
-isEC_returns_false_for_not_EC = TestCase $ assertEqual        "return false for isEagerCollapsible (isPrefixOf \"\"\") (isSuffixOf \"\"\") [\"1\",\"\"Hello\",\"\" simple\", \" test\"\", \"1\"]" False (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") ["1","\"Hello", "\"simple", " test\"","1"])
-isEC_returns_false_for_single_string = TestCase $ assertEqual "return false for whether or not a single string: [\"Hello World\"] is an eager collapsible"                                        False (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") ["\"Hello World\""])
-isEC_testList = TestList [isEC_returns_true_for_EC, isEC_returns_false_for_not_EC,isEC_returns_false_for_single_string]
-isECTests = runTestTT isEC_testList
+isECTests = testGroup "isEagerCollapsible" testList where
+    testList = 
+        [
+            isEC_returns_true_for_EC,
+            isEC_returns_false_for_not_EC,
+            isEC_returns_false_for_single_string
+        ]
+isEC_returns_true_for_EC = testCase name assertion where
+    name      = "return true for isEagerCollapsible (isPrefixOf \"\"\") (isSuffixOf \"\"\") [\"1\",\"\"Hello\",\" simple\", \" test\"\", \"1\"]"
+    assertion = assertEqual name assert func
+    assert    = True
+    func      = (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") [" simple", " test\"","1"])
+isEC_returns_false_for_not_EC = testCase name assertion where
+    name      = "return false for isEagerCollapsible (isPrefixOf \"\"\") (isSuffixOf \"\"\") [\"1\",\"\"Hello\",\"\" simple\", \" test\"\", \"1\"]"
+    assertion = assertEqual name assert func
+    assert    = False
+    func      = (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") ["1","\"Hello", "\"simple", " test\"","1"])
+isEC_returns_false_for_single_string = testCase name assertion where
+    name      = "return false for whether or not a single string: [\"Hello World\"] is an eager collapsible"
+    assertion = assertEqual name assert func
+    assert    = False
+    func      = (isEagerCollapsible (isPrefixOf "\"") (isSuffixOf "\"") ["\"Hello World\""])
+
+
 
 
 -- | Unit tests for Token.Util.EagerCollapsible.takeBetween as tB
-tB_returns_empty_list_for_empty_list_argument = TestCase $ assertEqual              "For takeBetween _ _ [] return []"                                                                          []              (takeBetween (1==) (1==) [])
-tB_returns_inclusive_list_of_EC = TestCase $ assertEqual                            "for an EC with beginCase (==1) and endCase (==1), return inclusive list of all elements between the two."  ([1,2,3,4,5,1]) (takeBetween ((==) 1) ((==) 1) [2,3,1,2,3,4,5,1,2,3,4])
-tB_returns_only_one_EC_from_list_with_multiple_separate_EC = TestCase $ assertEqual "for takeBetween (1==) (1==) [2,1,2,3,1,2,1,8,9,7,1,3] return only the first inclusive EC: [1,2,3,1]"       [1,2,3,1]       (takeBetween (1==) (1==) [2,1,2,3,1,2,1,8,9,7,1,3])
-tBTests = runTestTT $ TestList [tB_returns_empty_list_for_empty_list_argument, tB_returns_inclusive_list_of_EC,tB_returns_only_one_EC_from_list_with_multiple_separate_EC]
+tBTests = testGroup "takeBetween" testList where
+    testList = 
+        [
+            tB_returns_empty_list_for_empty_list_argument,
+            tB_returns_inclusive_list_of_EC,
+            tB_returns_only_one_EC_from_list_with_multiple_separate_EC
+        ]
+tB_returns_empty_list_for_empty_list_argument = testCase name assertion where
+    name = "tB empty list argument"
+    assertion = assertEqual desc assert func
+    desc = "For takeBetween _ _ [] return []"
+    assert = []
+    func = (takeBetween (1==) (1==) [])
+tB_returns_inclusive_list_of_EC = testCase name assertion where
+    name = "Return takeBetween list of EC list with one EC"
+    assertion = assertEqual desc assert func
+    desc = "for an EC with beginCase (==1) and endCase (==1), return inclusive list of all elements between the two."
+    assert = [1,2,3,4,5,1]
+    func = takeBetween ((==) 1) ((==) 1) [2,3,1,2,3,4,5,1,2,3,4]
+tB_returns_only_one_EC_from_list_with_multiple_separate_EC = testCase name assertion where
+    name = "One takeBetween from EC list with two or more EC"
+    assertion = assertEqual desc assert func
+    desc = "for takeBetween (1==) (1==) [2,1,2,3,1,2,1,8,9,7,1,3] return only the first inclusive EC: [1,2,3,1]"
+    assert = [1,2,3,1]
+    func = (takeBetween (1==) (1==) [2,1,2,3,1,2,1,8,9,7,1,3])
 
 
-
--- | run all tests for Token.Util.EagerCollapsible
-eagerCollapsibleTests = do
-    dropInfixTests
-    isECTests
-    tBTests
 
