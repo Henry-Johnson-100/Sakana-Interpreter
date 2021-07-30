e7aa6a76f68f9d603b318e61e76be48be1f10c5a
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-07-30 13:00:43 -0500

    misstyped in some of the tests

--- Test/LexerTest.hs
+++ Test/LexerTest.hs
@@ -36,15 +36,15 @@ tokenize_factorial = testCase name assertion where
     name      = "tokenize the factorial function"
     assertion = assertEqual d a f
     d         = "fish factorial >(n)> <( fin >( n == 0 , 1 )> n * factorial >( <( n - 1 )< )> )<"
-    a         = [Keyword K.Fish, Bracket (B.Send B.Open), Data (D.Id "n"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "n"), Operator O.Eq, Data (D.Int 0), Data (D.Punct ","), Data (D.Int 1), Bracket (B.Send B.Close), Data (D.Id "n"), Operator O.Mult, Data (D.Id "factorial"), Bracket (B.Send B.Open), Bracket (B.Return B.Open), Data (D.Id "n"), Operator O.Sub, Data (D.Int 1), Bracket (B.Return B.Close), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
+    a         = [Keyword K.Fish, Data (D.Id "factorial"), Bracket (B.Send B.Open), Data (D.Id "n"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "n"), Operator O.Eq, Data (D.Int 0), Data (D.Punct ","), Data (D.Int 1), Bracket (B.Send B.Close), Data (D.Id "n"), Operator O.Mult, Data (D.Id "factorial"), Bracket (B.Send B.Open), Bracket (B.Return B.Open), Data (D.Id "n"), Operator O.Sub, Data (D.Int 1), Bracket (B.Return B.Close), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
     f         = tokenize "fish factorial >(n)> <( fin >( n == 0 , 1 )> n * factorial >( <( n - 1 )< )> )<"
 
 tokenize_factorial_unoptimal_spacing = testCase name assertion where
     name      = "factorial function with no spacing between brackets, controls, operators, function or id calls"
     assertion = assertEqual d a f
-    d         = "fish factorial >(n)><(fin>(n==0,1)>n*factorial>(<(n=1)<)>)<"
-    a         = [Keyword K.Fish, Bracket (B.Send B.Open), Data (D.Id "n"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "n"), Operator O.Eq, Data (D.Int 0), Data (D.Punct ","), Data (D.Int 1), Bracket (B.Send B.Close), Data (D.Id "n"), Operator O.Mult, Data (D.Id "factorial"), Bracket (B.Send B.Open), Bracket (B.Return B.Open), Data (D.Id "n"), Operator O.Sub, Data (D.Int 1), Bracket (B.Return B.Close), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
-    f         = tokenize "fish factorial >(n)><(fin>(n==0,1)>n*factorial>(<(n=1)<)>)<"
+    d         = "fish factorial >(n)><(fin>(n==0,1)>n*factorial>(<(n-1)<)>)<"
+    a         = [Keyword K.Fish, Data (D.Id "factorial"), Bracket (B.Send B.Open), Data (D.Id "n"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "n"), Operator O.Eq, Data (D.Int 0), Data (D.Punct ","), Data (D.Int 1), Bracket (B.Send B.Close), Data (D.Id "n"), Operator O.Mult, Data (D.Id "factorial"), Bracket (B.Send B.Open), Bracket (B.Return B.Open), Data (D.Id "n"), Operator O.Sub, Data (D.Int 1), Bracket (B.Return B.Close), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
+    f         = tokenize "fish factorial >(n)><(fin>(n==0,1)>n*factorial>(<(n-1)<)>)<"
 
 tokenizeer_also_auto_consolidates_string_data = testCase name assertion where
     name      = "groups of Data should be auto consolidated into one Data String if possible"
