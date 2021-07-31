5f58c2a64c2af114373acfa0d0b68fc3315748f6
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-07-31 13:44:27 -0500

    Added a tree data structure and functor instance

--- app/AbstractSyntaxTree.hs
+++ app/AbstractSyntaxTree.hs
@@ -3,3 +3,11 @@ module AbstractSyntaxTree(
 ) where
 
 import Lexer
+
+data Tree a = Empty | Leaf a (Tree a) (Tree a) deriving (Show,Read,Eq)
+
+testTree = (Leaf 1 (Leaf 2 (Leaf 3 Empty Empty) Empty) (Leaf 4 (Leaf 5 Empty Empty) Empty))
+
+instance Functor Tree where
+    fmap _ Empty                   = Empty
+    fmap f (Leaf a (left) (right)) = (Leaf (f a) (fmap f left) (fmap f right))
