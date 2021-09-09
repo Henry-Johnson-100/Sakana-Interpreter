34228a7c43c0d5d75b1732d49854d262b6e610d0
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-09 09:54:03 -0500

    Added the hook and migrate keywords

--- app/Token/Keyword.hs
+++ app/Token/Keyword.hs
@@ -5,19 +5,23 @@ fromKeyword,
 repr
 ) where
 
-data Keyword = Fish | Route | School | Shoal deriving (Show, Read, Eq, Ord) --idk if Ord is really necessary
+data Keyword = Hook | Fish | Route | School | Shoal | Migrate deriving (Show, Read, Eq, Ord) --idk if Ord is really necessary
 
 repr :: [String]
-repr = ["route","fish","school","shoal"]  
+repr = ["route","fish","school","shoal", "migrate", "hook"]  
 
 readKeyword :: String -> Keyword
-readKeyword "fish"   = Fish
-readKeyword "route"  = Route
-readKeyword "school" = School
-readKeyword "shoal"  = Shoal
+readKeyword "fish"    = Fish
+readKeyword "route"   = Route
+readKeyword "school"  = School
+readKeyword "shoal"   = Shoal
+readKeyword "hook"    = Hook
+readKeyword "migrate" = Migrate
 
 fromKeyword :: Keyword -> String
-fromKeyword Fish   = "fish"
-fromKeyword Route  = "route"
-fromKeyword School = "school"
-fromKeyword Shoal  = "shoal"
+fromKeyword Fish    = "fish"
+fromKeyword Route   = "route"
+fromKeyword School  = "school"
+fromKeyword Shoal   = "shoal"
+fromKeyword Migrate = "migrate"
+fromKeyword Hook    = "hook"
