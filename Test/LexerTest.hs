77bbd2086b6be4b57dfa3bb83aa66dfaaa592b60
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-09 11:42:34 -0500

    Added a new test to ensure that comments are ignored by anything prior to the tokenizer

--- Test/LexerTest.hs
+++ Test/LexerTest.hs
@@ -22,7 +22,8 @@ tests = testGroup "Lexer Tests" testList where
             tokenize_factorial,
             tokenize_factorial_unoptimal_spacing,
             tokenizer_also_auto_consolidates_string_data,
-            new_keywords_and_type_symbol_are_tokenized_correctly
+            new_keywords_and_type_symbol_are_tokenized_correctly,
+            comments_are_ignored
         ]
 
 -- | Lexer.tokenize tests as t
@@ -60,3 +61,10 @@ new_keywords_and_type_symbol_are_tokenized_correctly = testCase name assertion w
     d         = "migrate >(Functor)> school List >(x, xs >::> List)> >(Functor)> <( school List.Empty >()> <( <()< )< fish Functor.fmap >( hook >()>, f)> <( fin >( null >(xs)>, List.Singleton >(f >(x)> )> )> cons >( fmap >(xs,f)>, f >(x)> )> )< )<"
     a         = [Keyword K.Migrate, Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Keyword K.School, Data (D.Id "List"), Bracket (B.Send B.Open), Data (D.Id "x"), Data (D.Punct ","), Data (D.Id "xs"), Operator O.ExplType, Data (D.Id "List"), Bracket (B.Send B.Close), Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Keyword K.School, Data (D.Id "List.Empty"), Bracket (B.Send B.Open), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Bracket (B.Return B.Open), Bracket (B.Return B.Close), Bracket (B.Return B.Close), Keyword K.Fish, Data (D.Id "Functor.fmap"), Bracket (B.Send B.Open), Keyword K.Hook, Bracket (B.Send B.Open), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "null"), Bracket (B.Send B.Open), Data (D.Id "xs"), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "List.Singleton"), Bracket (B.Send B.Open), Data (D.Id "f"), Bracket (B.Send B.Open), Data (D.Id "x"), Bracket (B.Send B.Close), Bracket (B.Send B.Close), Bracket (B.Send B.Close), Data (D.Id "cons"), Bracket (B.Send B.Open), Data (D.Id "fmap"), Bracket (B.Send B.Open), Data (D.Id "xs"), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Open), Data (D.Id "x"), Bracket (B.Send B.Close), Bracket (B.Send B.Close), Bracket (B.Return B.Close), Bracket (B.Return B.Close)]
     f         = tokenize "migrate >(Functor)> school List >(x, xs >::> List)> >(Functor)> <( school List.Empty >()> <( <()< )< fish Functor.fmap >( hook >()>, f)> <( fin >( null >(xs)>, List.Singleton >(f >(x)> )> )> cons >( fmap >(xs,f)>, f >(x)> )> )< )<"
+
+comments_are_ignored = testCase name assertion where
+    name      = "Comments are ignored by tokenizer"
+    assertion = assertEqual d a f
+    d         = "Comments are ignored for all future computations, so they should not be represented by the tokenizer"
+    a         = [Keyword K.Migrate, Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Keyword K.School, Data (D.Id "List"), Bracket (B.Send B.Open), Data (D.Id "x"), Data (D.Punct ","), Data (D.Id "xs"), Operator O.ExplType, Data (D.Id "List"), Bracket (B.Send B.Close), Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Keyword K.Fish, Data (D.Id "Functor.fmap"), Bracket (B.Send B.Open), Keyword K.Hook, Bracket (B.Send B.Open), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "null"), Bracket (B.Send B.Open), Data (D.Id "xs"), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
+    f         = tokenize "migrate >(Functor)> school List >(x, xs >::> List)> >(Functor)> <( /*school List.Empty >()> <( <()< )<*/ fish Functor.fmap >( hook >()>, f)> <( fin >( null >(xs)>/*, List.Singleton >(f >(x)> )> )> cons >( fmap >(xs,f)>, f >(x)> )> )<*/ )<"
