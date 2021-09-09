96ee0a6520f1c711c602b6e0637ad898d6816e69
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-09 10:47:55 -0500

    Added a Comment Data type and created a general consolidateEagerCollapsibleData function

--- app/Token/Data.hs
+++ app/Token/Data.hs
@@ -1,6 +1,6 @@
 module Token.Data (
     Data(..),
-    consolidateStrings,
+    consolidateEagerCollapsibleData,
     readData,
     fromData,
     punctRepr
@@ -11,19 +11,21 @@ import Data.Char
 import Data.List
 import Token.Util.Like
 import Token.Util.EagerCollapsible
+import Token.Util.String (strip)
 
 
-data Data = Int Int | Float Float | String String | Boolean Bool | Id String | Punct String | Other String deriving (Show,Read,Eq,Ord)
+data Data = Int Int | Float Float | String String | Boolean Bool | Id String | Punct String | Other String | Comment String deriving (Show,Read,Eq,Ord)
 
 
 instance Like Data where
-    (Int a)     `like`    (Int b)     = True
+    (Int _)     `like`    (Int _)     = True
     (Float _)   `like`    (Float _)   = True
-    (String a)  `like`    (String b)  = True
-    (Boolean a) `like`    (Boolean b) = True
+    (String _)  `like`    (String _)  = True
+    (Boolean _) `like`    (Boolean _) = True
     (Id _)      `like`    (Id _)      = True
     (Punct _)   `like`    (Punct _)   = True
-    (Other a)   `like`    (Other b)   = True
+    (Other _)   `like`    (Other _)   = True
+    (Comment _) `like`    (Comment _) = True
     _           `like`    _           = False
     a           `notLike` b           = not $ like a b
 
@@ -38,15 +40,22 @@ fromData (Boolean a) = show a
 fromData (Id a)      = a
 fromData (Punct a)   = a
 fromData (Other a)   = a
+fromData (Comment a) = a
 
-
+{-
 convertToDataString :: Data -> Data
 convertToDataString d = String (fromData d)
+
+convertToDataComment :: Data -> Data
+convertToDataComment d = Comment (fromData d)
 
 
 mapToString :: [Data] -> [Data]
 mapToString xs = map (convertToDataString) xs
 
+mapToComment :: [Data] -> [Data]
+mapToComment xs = map (convertToDataComment) xs
+-}
 
 allDigits :: String -> Bool
 allDigits str = all (isDigit) str
@@ -75,17 +84,41 @@ couldBeId str = maybeContainsSnakeCaseOrDot && isOtherWiseAllAlpha && containsNo
         isOtherWiseAllAlpha         = (allAlpha (filter (\x -> ('.' /= x) && ('_' /= x)) str))
         containsNoDigits            = not $ any (isDigit) str
 
+--isEagerCollapsibleDataTypePrefix :: (String -> Data) -> (Data -> Bool)
+isEagerCollapsibleDataTypePrefix (String _)  = isStringPrefix
+isEagerCollapsibleDataTypePrefix (Comment _) = isCommentPrefix
+--isEagerCollapsibleDataTypePrefix _       = False
+
+isEagerCollapsibleDataTypeSuffix (String _)  = isStringSuffix
+isEagerCollapsibleDataTypeSuffix (Comment _) = isCommentSuffix
 
 isStringPrefix :: Data -> Bool
 isStringPrefix (String a) = ((isPrefixOf "\"" a) && (not $ isSuffixOf "\"" a)) || (length a == 1)
 isStringPrefix _          = False
 
-
 isStringSuffix :: Data -> Bool
 isStringSuffix (String a) = ((isSuffixOf "\"" a) && (not $ isPrefixOf "\"" a)) || (length a == 1)
 isStringSuffix _          = False
 
+isCommentPrefix :: Data -> Bool
+isCommentPrefix (Comment a) = isPrefixOf "/*" a
+isCommentPrefix _         = False
 
+isCommentSuffix :: Data -> Bool
+isCommentSuffix (Comment a) = isSuffixOf "*/" a
+isCommentSuffix _         = False
+
+consolidateEagerCollapsibleData :: [Data] -> [Data]
+consolidateEagerCollapsibleData [] = []
+consolidateEagerCollapsibleData (d:ds)
+    | isStringPrefix  d && isEagerCollapsible isStringPrefix  isStringSuffix  (d:ds) = (mapToConsolidatedData (String "") (d:ds))  ++ consolidateEagerCollapsibleData (dropBetween (isEagerCollapsibleDataTypePrefix (String ""))  (isEagerCollapsibleDataTypeSuffix (String ""))  (d:ds))
+    | isCommentPrefix d && isEagerCollapsible isCommentPrefix isCommentSuffix (d:ds) = (mapToConsolidatedData (Comment "") (d:ds)) ++ consolidateEagerCollapsibleData (dropBetween (isEagerCollapsibleDataTypePrefix (Comment "")) (isEagerCollapsibleDataTypeSuffix (Comment "")) (d:ds))
+    | otherwise                                                                      = d : consolidateEagerCollapsibleData ds
+    where
+        mapTakeBetween        emptyDataType xs = map (\d -> emptyDataType (fromData d)) $ takeBetween (isEagerCollapsibleDataTypePrefix emptyDataType) (isEagerCollapsibleDataTypeSuffix emptyDataType) xs
+        mapToConsolidatedData emptyDataType xs = emptyDataType (concat (map (fromData) (mapTakeBetween emptyDataType xs))) : []
+
+{-
 consolidateStrings :: [Data] -> [Data]
 consolidateStrings [] = []
 consolidateStrings (d:ds)
@@ -95,17 +128,26 @@ consolidateStrings (d:ds)
         mapTakeBetween xs = mapToString $ takeBetween isStringPrefix isStringSuffix xs
         mapToConsolidatedStringData xs = String (concat ( map (fromData) (mapTakeBetween xs))) : []
 
-
-readData :: String -> Data
-readData pstr
-    | null str                        = Other ""
-    | allAlphaNum $ strip str         = Other str
-    | allDigits $ strip str           = Int (read str :: Int)
-    | isFloatStr $ strip str          = Float (read str :: Float)
-    | allPunct $ strip str            = Punct str
-    | elem '\"' str                   = String str --Don't like this one
-    | str == "True" || str == "False" = Boolean ( read str :: Bool )
-    | couldBeId $ strip str           = Id str
-    | otherwise                       = Other str
+consolidateComments :: [Data] -> [Data] --Type error on first guard btw
+consolidateComments [] = []
+consolidateComments ds
+    | isCommentPrefix ds && isEagerCollapsible isCommentPrefix isCommentSuffix ds = (mapToConsolidatedCommentData (ds)) ++ consolidateComments (dropBetween isCommentPrefix isCommentSuffix ds)
+    | otherwise                                                                   = (head ds) : consolidateComments (tail ds)
     where
-        str = strip pstr
+        mapTakeBetween xs = mapToComment $ takeBetween isCommentPrefix isCommentSuffix xs
+        mapToConsolidatedCommentData xs = Comment (concat (map (fromData) (mapTakeBetween xs))) : []
+-}
+readData :: String -> Data --These guards are order dependent which is annoying
+readData paddedStr
+    | null str                                   = Other ""
+    | isPrefixOf "/*" str || isSuffixOf "*/" str = Comment str
+    | allAlphaNum $ strip str                    = Other str
+    | allDigits $ strip str                      = Int (read str :: Int)
+    | isFloatStr $ strip str                     = Float (read str :: Float)
+    | allPunct $ strip str                       = Punct str
+    | elem '\"' str                              = String str --Don't like this one
+    | str == "True" || str == "False"            = Boolean ( read str :: Bool )
+    | couldBeId $ strip str                      = Id str
+    | otherwise                                  = Other str
+    where
+        str = strip paddedStr
