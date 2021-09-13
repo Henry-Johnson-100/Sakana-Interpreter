dd27430abdcb3de5fb85804d425ae85e55b74da8
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-13 18:37:30 -0500

    Wrote some implementation will hopefully work, haven't tried compiling it yet

--- app/Token/Util/NestedCollapsible.hs
+++ app/Token/Util/NestedCollapsible.hs
@@ -1,6 +1,7 @@
 module Token.Util.NestedCollapsible (
     isCompleteNestedCollapsible,
-    hasNestedCollapsible
+    hasNestedCollapsible,
+    takeNest
 ) where
 
 import Data.List
@@ -12,6 +13,23 @@ isCompleteNestedCollapsible beginCase endCase xs = (fst terminations) == (snd te
 hasNestedCollapsible :: (a -> Bool) -> (a -> Bool) -> [a] -> Bool
 hasNestedCollapsible beginCase endCase xs = all (0 < ) [fst terminations, snd terminations] where
     terminations = numberOfTerminations beginCase endCase 0 0 xs
+
+takeNest :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
+takeNest _ _ [] = []
+takeNest beginCase endCase (x:xs)
+    | not (hNC (x:xs))  = []
+    | not (iCNC (x:xs)) = takeNest beginCase endCase xs
+    | otherwise         = takeUntilTerminationLevel endCase (snd (numberOfTerminations beginCase endCase (x:xs))) (dropWhile (\xx -> not (beginCase xx)) (x:xs))
+    where
+        hNC ncs = hasNestedCollapsible beginCase endCase ncs
+        iCNC ncs = isCompleteNestedCollapsible beginCase endCase ncs
+
+takeUntilTerminationLevel :: (a -> Bool) -> Int -> [a] -> [a]
+takeUntilTerminationLevel _ 0 _ = []
+takeUntilTerminationLevel termCase termLevel (x:xs)
+    | null xs = [] --should hopefully never reach this
+    | termCase x = x : takeUntilTerminationLevel termCase (termLevel - 1) xs
+    | otherwise  = x : takeUntilTerminationLevel termCase termLevel xs
 
 numberOfTerminations :: (a -> Bool) -> (a -> Bool) -> Int -> Int -> [a] -> (Int,Int)
 numberOfTerminations _ _ begins ends [] = (begins, ends)
