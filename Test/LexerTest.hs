51c5866e6d2fc0f83b9621401033d68d0750d6b7
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-14 14:47:59 -0500

    Added a test

--- Test/LexerTest.hs
+++ Test/LexerTest.hs
@@ -23,7 +23,8 @@ tests = testGroup "Lexer Tests" testList where
             tokenize_factorial_unoptimal_spacing,
             tokenizer_also_auto_consolidates_string_data,
             new_keywords_and_type_symbol_are_tokenized_correctly,
-            comments_are_ignored
+            comments_are_ignored,
+            nested_comments_are_treated_as_NC_and_not_EC_and_ignored
         ]
 
 -- | Lexer.tokenize tests as t
@@ -68,3 +69,10 @@ comments_are_ignored = testCase name assertion where
     d         = "Comments are ignored for all future computations, so they should not be represented by the tokenizer"
     a         = [Keyword K.Migrate, Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Keyword K.School, Data (D.Id "List"), Bracket (B.Send B.Open), Data (D.Id "x"), Data (D.Punct ","), Data (D.Id "xs"), Operator O.ExplType, Data (D.Id "List"), Bracket (B.Send B.Close), Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Keyword K.Fish, Data (D.Id "Functor.fmap"), Bracket (B.Send B.Open), Keyword K.Hook, Bracket (B.Send B.Open), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "null"), Bracket (B.Send B.Open), Data (D.Id "xs"), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
     f         = tokenize "migrate >(Functor)> school List >(x, xs >::> List)> >(Functor)> <( /*school List.Empty >()> <( <()< )<*/ fish Functor.fmap >( hook >()>, f)> <( fin >( null >(xs)>/*, List.Singleton >(f >(x)> )> )> cons >( fmap >(xs,f)>, f >(x)> )> )<*/ )<"
+
+nested_comments_are_treated_as_NC_and_not_EC_and_ignored = testCase name assertion where
+    name      = "Comments can be nested, it is only necessary to explicitly find and ignore the top level comments"
+    assertion = assertEqual d a f
+    d         = "Comments should be possible to nest and should still all be ignored by the lexer"
+    a         = [Keyword K.Migrate, Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Keyword K.School, Data (D.Id "List"), Bracket (B.Send B.Open), Data (D.Id "x"), Data (D.Punct ","), Data (D.Id "xs"), Operator O.ExplType, Data (D.Id "List"), Bracket (B.Send B.Close), Bracket (B.Send B.Open), Data (D.Id "Functor"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Keyword K.Fish, Data (D.Id "Functor.fmap"), Bracket (B.Send B.Open), Keyword K.Hook, Bracket (B.Send B.Open), Bracket (B.Send B.Close), Data (D.Punct ","), Data (D.Id "f"), Bracket (B.Send B.Close), Bracket (B.Return B.Open), Control C.Fin, Bracket (B.Send B.Open), Data (D.Id "null"), Bracket (B.Send B.Open), Data (D.Id "xs"), Bracket (B.Send B.Close), Bracket (B.Return B.Close)]
+    f         = tokenize "migrate >(Functor)> school List >(x, xs >::> List)> >(Functor)> <( /* /*bruh bruh bruh*/school List.Empty >()> <( <()< )<*/ fish Functor.fmap >( hook >()>, f)> <( fin >( null >(xs)>/*, List.Singleton /* bruh bruh bruh /*bruh bruh bruh*/ bruh*/>(f >(x)> )> )> cons >( fmap >(xs,f)>, f >(x)> )> )<*/ )<"
