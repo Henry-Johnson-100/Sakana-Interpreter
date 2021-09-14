302ed649f86f2da5eb02514900cbe90cae096d91
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-13 19:19:08 -0500

    Implemented takeNest, all current tests passing, will add more tomorrow probably

--- app/Token/Util/NestedCollapsible.hs
+++ app/Token/Util/NestedCollapsible.hs
@@ -17,19 +17,24 @@ hasNestedCollapsible beginCase endCase xs = all (0 < ) [fst terminations, snd te
 takeNest :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
 takeNest _ _ [] = []
 takeNest beginCase endCase (x:xs)
-    | not (hNC (x:xs))  = []
-    | not (iCNC (x:xs)) = takeNest beginCase endCase xs
-    | otherwise         = takeUntilTerminationLevel endCase (snd (numberOfTerminations beginCase endCase 0 0 (x:xs))) (dropWhile (\xx -> not (beginCase xx)) (x:xs))
+    | not (hNC (x:xs))                        = []
+    | iCNC (x:xs)                             = takeNest beginCase endCase xs
+    | (fst terminations) > (snd terminations) = takeNest beginCase endCase xs
+    | otherwise                               = takeUntilTerminationLevel endCase (minTuple terminations) (dropWhile (\xx -> not (beginCase xx)) (x:xs))
     where
         hNC ncs = hasNestedCollapsible beginCase endCase ncs
         iCNC ncs = isCompleteNestedCollapsible beginCase endCase ncs
+        terminations = numberOfTerminations beginCase endCase 0 0 (x:xs)
 
 takeUntilTerminationLevel :: (a -> Bool) -> Int -> [a] -> [a]
 takeUntilTerminationLevel _ 0 _ = []
+takeUntilTerminationLevel _ _ [] = []
 takeUntilTerminationLevel termCase termLevel (x:xs)
-    | null xs = [] --should hopefully never reach this
     | termCase x = x : takeUntilTerminationLevel termCase (termLevel - 1) xs
     | otherwise  = x : takeUntilTerminationLevel termCase termLevel xs
+
+minTuple :: (Int, Int) -> Int
+minTuple (a, b) = minimum [a,b]
 
 numberOfTerminations :: (a -> Bool) -> (a -> Bool) -> Int -> Int -> [a] -> (Int,Int)
 numberOfTerminations _ _ begins ends [] = (begins, ends)
