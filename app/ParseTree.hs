c29a1195ac2b881de6b69be8df08ea7a79fe49ff
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-15 14:45:10 -0500

    yet another ParseTree data mockup

--- app/ParseTree.hs
+++ app/ParseTree.hs
@@ -9,19 +9,14 @@ import Token.Control
 import Token.Data
 import Token.Keyword
 import Token.Operator
-
+import Token.Util.NestedCollapsible
 import Data.List
 
-data ParseTree a = Empty | Node a (ParseTree a) (ParseTree a) deriving (Show, Read, Eq)
-
+data ParseTree a = Empty | Node a (ParseTree a) [ParseTree a] deriving (Show, Read, Eq)
 type TokenTree = ParseTree Token
 
 node :: a -> ParseTree a
-node x = Node x Empty Empty
-
-instance Functor ParseTree where
-    fmap _ Empty                  = Empty
-    fmap f (Node base left right) = Node (f base) (fmap f left) (fmap f right)
+node x = Node x Empty []
 
 
 generateParseTree :: [Token] -> ParseTree Token
