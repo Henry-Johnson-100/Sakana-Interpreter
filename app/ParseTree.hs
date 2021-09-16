82b926411e474ec0676acc2324296845521cb769
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-16 15:01:19 -0500

    Trying to write some stuff, not going so well. Going to checkout a new branch and develop a few other data structures

--- app/ParseTree.hs
+++ app/ParseTree.hs
@@ -22,6 +22,11 @@ instance Functor ParseTree where
     fmap f (ParseTree b cs) = ParseTree (f b) (map (\c -> fmap f c) cs)
 
 
+-- sendBracketNC   = NCCase ((Bracket (Send Open)) ==) ((Bracket (Send Close)) ==)
+bracketNC = NCCase (\x -> any (x ==) [Bracket (Send Open), Bracket (Return Open)]) (\x -> any (x ==) [Bracket (Send Close), Bracket (Return Close)])
+-- returnBracketNC = NCCase ((Bracket (Return Open)) ==) ((Bracket (Return Close)) ==)
+
+
 nullTree :: ParseTree a -> Bool
 nullTree Empty = True
 nullTree _     = False
@@ -31,9 +36,17 @@ tree :: a -> ParseTree a
 tree x = ParseTree x []
 
 
-append :: a -> ParseTree a -> ParseTree a
-append x Empty            = tree x
-append x (ParseTree b cs) = ParseTree b ((tree x) : cs)
+-- append :: a -> ParseTree a -> ParseTree a
+-- append x Empty            = tree x
+-- append x (ParseTree b cs) = ParseTree b ((tree x) : cs)
+
+
+appendChild :: ParseTree a -> ParseTree a -> ParseTree a
+appendChild (ParseTree b cs) pt = ParseTree b (cs ++ [pt])
+
+
+extendChildren :: ParseTree a -> [ParseTree a] -> ParseTree a
+extendChildren (ParseTree b cs) pts = ParseTree b (cs ++ pts)
 
 
 fPrintTree :: (Show a) => Int -> ParseTree a -> String
@@ -45,4 +58,13 @@ ioPrintTree t = putStrLn $ fPrintTree 0 t
 
 
 generateParseTree :: [Token] -> ParseTree Token
-generateParseTree ts = Empty
+generateParseTree [] = Empty
+generateParseTree ts = generateParseTree' Empty ts where
+    generateParseTree' :: ParseTree Token -> [Token] -> ParseTree Token
+    generateParseTree' pt [] = pt
+    generateParseTree' Empty (t:ts) = generateParseTree' (tree t) ts
+    generateParseTree' pt ts
+        | isBracketPrefixed ts = generateParseTree' (extendChildren pt ()) (partThd (part ts))
+        where
+            isBracketPrefixed ts' = nestedCollapsibleIsPrefixOf bracketNC ts'
+            part ts' = breakByNest bracketNC ts'
