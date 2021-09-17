5566b1bb472cd89640272441ded1fd97f64b7d8b
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-17 14:01:10 -0500

    Made some general rules for some keywords

--- app/Grammar/GrammarRules.hs
+++ app/Grammar/GrammarRules.hs
@@ -1,10 +1,12 @@
 module Grammar.GrammarRules (
-GrammarRule(..)
+GrammarRule(..),
+rule
 ) where
 
 import Lexer
-import Token.Bracket (Bracket)
-import Token.Util.NestedCollapsible (NCCase)
+import Token.Bracket
+import Token.Keyword 
+import Token.Util.NestedCollapsible
 
 
 data BracketRule = BracketRule {
@@ -16,12 +18,38 @@ data BracketRule = BracketRule {
 
 
 instance Show BracketRule where
-    show br = unwords ["Type:",show (brType br),",","is required: ", show (isRequired br)]
+    show br = unwords ["\nType:",show (brType br),"\n\tis required:", show (isRequired br), "\n\tNumber of bracket sets:", show (numSets br)]
 
 
 instance Eq BracketRule where
     (==) (BracketRule brtypex isRx numSetsx _) (BracketRule brtypey isRy numSety _) = brtypex == brtypey && isRx == isRy && numSetsx == numSety
     (/=) brx                                   bry                                  = not (brx == bry)
+
+
+generalOpenBRRule :: BracketRule
+generalOpenBRRule = BracketRule {
+        brType = Send Open,
+        isRequired = False,
+        numSets    = 1,
+        ncCase     = NCCase ((Bracket (Send Open)) == ) ((Bracket (Send Close)) == )
+    }
+
+
+generalCloseBRRule :: BracketRule
+generalCloseBRRule = BracketRule {
+        brType = Return Open,
+        isRequired = True,
+        numSets = 1,
+        ncCase = NCCase ((Bracket (Return Open)) == ) ((Bracket (Return Close)) == )
+    }
+
+
+flipIsRequired :: BracketRule -> BracketRule
+flipIsRequired br = BracketRule (brType br) (not (isRequired br)) (numSets br) (ncCase br)
+
+
+setNumSets :: BracketRule -> Int -> BracketRule
+setNumSets br n = BracketRule (brType br) (isRequired br) (n) (ncCase br)
 
 
 data GrammarRule = NoGR | GrammarRule {
@@ -32,7 +60,7 @@ data GrammarRule = NoGR | GrammarRule {
 
 
 instance Show GrammarRule where
-    show gr = unwords ["Type:", show  (grType gr), "send Rule:", show (sendRule gr), "return Rule:", show (returnRule gr)]
+    show gr = unwords ["Type:", show  (grType gr), "\n\nsend Rule:", show (sendRule gr), "\n\nreturn Rule:", show (returnRule gr)]
 
 
 instance Eq GrammarRule where
@@ -42,4 +70,8 @@ instance Eq GrammarRule where
 
 
 rule :: Token -> GrammarRule
+rule (Keyword Fish)   = GrammarRule (Keyword Fish)   generalOpenBRRule                                 generalCloseBRRule
+rule (Keyword Route)  = GrammarRule (Keyword Route)  generalOpenBRRule                                 generalCloseBRRule
+rule (Keyword School) = GrammarRule (Keyword School) (flipIsRequired (setNumSets generalOpenBRRule 2)) generalCloseBRRule
+rule (Keyword Shoal)  = GrammarRule (Keyword Shoal)  (flipIsRequired (setNumSets generalOpenBRRule 0)) generalCloseBRRule
 rule _ = NoGR
