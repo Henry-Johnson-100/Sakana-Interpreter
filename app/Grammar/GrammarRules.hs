018aab030664c7ddab62d5c7d87afc9793b11e05
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-17 14:51:10 -0500

    Changed grammar rules to hold a list of bracket rules and some other tweaks to show instances

--- app/Grammar/GrammarRules.hs
+++ app/Grammar/GrammarRules.hs
@@ -6,24 +6,24 @@ rule
 import Lexer
 import Token.Bracket
 import Token.Keyword 
+import Token.Control
 import Token.Util.NestedCollapsible
 
 
 data BracketRule = BracketRule {
     brType     :: Bracket,
     isRequired :: Bool,
-    numSets    :: Int,
     ncCase     :: NCCase Token,
     argSets    :: Int
 }
 
 
 instance Show BracketRule where
-    show br = unwords ["\nType:",show (brType br),"\n\tis required:", show (isRequired br), "\n\tNumber of bracket sets:", show (numSets br)]
+    show br = unwords ["\nType:",show (brType br),"\n\tis required:", show (isRequired br), "\n\tNumber of arguments:", (if argsRequired br then show (argSets br) else "Optional")]
 
 
 instance Eq BracketRule where
-    (==) (BracketRule brtypex isRx numSetsx _ argSetsx) (BracketRule brtypey isRy numSety _ argSetsy) = brtypex == brtypey && isRx == isRy && numSetsx == numSety && argSetsx == argSetsy
+    (==) (BracketRule brtypex isRx _ argSetsx) (BracketRule brtypey isRy _ argSetsy) = brtypex == brtypey && isRx == isRy && argSetsx == argSetsy
     (/=) brx                                   bry                                  = not (brx == bry)
 
 
@@ -31,7 +31,6 @@ generalOpenBRRule :: BracketRule
 generalOpenBRRule = BracketRule {
         brType     = Send Open,
         isRequired = False,
-        numSets    = 1,
         ncCase     = NCCase ((Bracket (Send Open)) == ) ((Bracket (Send Close)) == ),
         argSets    = -1
     }
@@ -41,18 +40,13 @@ generalCloseBRRule :: BracketRule
 generalCloseBRRule = BracketRule {
         brType     = Return Open,
         isRequired = True,
-        numSets    = 1,
         ncCase     = NCCase ((Bracket (Return Open)) == ) ((Bracket (Return Close)) == ),
-        argSets    = -1
+        argSets    = 1
     }
 
 
 flipIsRequired :: BracketRule -> BracketRule
 flipIsRequired br = br {isRequired = not (isRequired br)}
-
-
-setNumSets :: BracketRule -> Int -> BracketRule
-setNumSets br n = br {numSets = n}
 
 
 setArgSets :: BracketRule -> Int -> BracketRule
@@ -65,24 +59,25 @@ argsRequired br = (argSets br) >= 0
 
 data GrammarRule = NoGR | GrammarRule {
     grType     :: Token,
-    sendRule   :: BracketRule,
-    returnRule :: BracketRule
+    sendRules   :: [BracketRule],
+    returnRules :: [BracketRule]
 }
 
 
 instance Show GrammarRule where
-    show gr = unwords ["Type:", show  (grType gr), "\n\nsend Rule:", show (sendRule gr), "\n\nreturn Rule:", show (returnRule gr)]
+    show gr = unwords $ ["Type:", show  (grType gr), "\n\nsend Rule:"] ++ map show (sendRules gr) ++ ["\n\nreturn Rule:"] ++ map show (returnRules gr)
 
 
 instance Eq GrammarRule where
     (==) NoGR NoGR = True
-    (==) grx  gry  = (grType grx) == (grType gry) && (sendRule grx) == (sendRule gry) && (returnRule grx) == (returnRule gry)
+    (==) grx  gry  = (grType grx) == (grType gry) && (sendRules grx) == (sendRules gry) && (returnRules grx) == (returnRules gry)
     (/=) grx  gry  = not (grx == gry)
 
 
 rule :: Token -> GrammarRule
-rule (Keyword Fish)   = GrammarRule (Keyword Fish)   generalOpenBRRule                                 generalCloseBRRule
-rule (Keyword Route)  = GrammarRule (Keyword Route)  generalOpenBRRule                                 generalCloseBRRule
-rule (Keyword School) = GrammarRule (Keyword School) (flipIsRequired (setNumSets generalOpenBRRule 2)) generalCloseBRRule
-rule (Keyword Shoal)  = GrammarRule (Keyword Shoal)  (flipIsRequired (setNumSets generalOpenBRRule 0)) generalCloseBRRule
+rule (Keyword Fish)   = GrammarRule (Keyword Fish)   [generalOpenBRRule]                                   [generalCloseBRRule]
+rule (Keyword Route)  = GrammarRule (Keyword Route)  [generalOpenBRRule]                                   [generalCloseBRRule]
+rule (Keyword School) = GrammarRule (Keyword School) [flipIsRequired generalOpenBRRule, generalOpenBRRule] [generalCloseBRRule]
+rule (Keyword Shoal)  = GrammarRule (Keyword Shoal)  [setArgSets generalOpenBRRule 0]                      [generalCloseBRRule]
+rule (Control Fin)    = GrammarRule (Control Fin)    [flipIsRequired (setArgSets generalOpenBRRule 3)]     [(flipIsRequired (setArgSets generalCloseBRRule 0))]
 rule _ = NoGR
