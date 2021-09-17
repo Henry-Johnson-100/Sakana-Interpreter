98ed9c80e0b8397aed48d3ed56f37fb346aad8f5
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-17 15:48:00 -0500

    Drafting some grammar validation functions

--- app/Grammar/GrammarRules.hs
+++ app/Grammar/GrammarRules.hs
@@ -59,6 +59,7 @@ argsRequired br = (argSets br) >= 0
 
 data GrammarRule = NoGR | GrammarRule {
     grType     :: Token,
+    idRequired :: Bool,
     sendRules   :: [BracketRule],
     returnRules :: [BracketRule]
 }
@@ -66,19 +67,48 @@ data GrammarRule = NoGR | GrammarRule {
 
 instance Show GrammarRule where
     show NoGR = "NoGR"
-    show gr = unwords $ ["Type:", show  (grType gr), "\n\nsend Rule:"] ++ map show (sendRules gr) ++ ["\n\nreturn Rule:"] ++ map show (returnRules gr)
+    show gr = unwords $ ["Type:", show  (grType gr), "\nRequires ID:", show (idRequired gr) ,"\n\nsend Rules:"] ++ map show (sendRules gr) ++ ["\n\nreturn Rules:"] ++ map show (returnRules gr)
 
 
 instance Eq GrammarRule where
     (==) NoGR NoGR = True
-    (==) grx  gry  = (grType grx) == (grType gry) && (sendRules grx) == (sendRules gry) && (returnRules grx) == (returnRules gry)
+    (==) grx  gry  = (grType grx) == (grType gry) && (sendRules grx) == (sendRules gry) && (returnRules grx) == (returnRules gry) && (idRequired grx) == (idRequired gry)
     (/=) grx  gry  = not (grx == gry)
 
 
 rule :: Token -> GrammarRule
-rule (Keyword Fish)   = GrammarRule (Keyword Fish)   [generalOpenBRRule]                                   [generalCloseBRRule]
-rule (Keyword Route)  = GrammarRule (Keyword Route)  [generalOpenBRRule]                                   [generalCloseBRRule]
-rule (Keyword School) = GrammarRule (Keyword School) [flipIsRequired generalOpenBRRule, generalOpenBRRule] [generalCloseBRRule]
-rule (Keyword Shoal)  = GrammarRule (Keyword Shoal)  [setArgSets generalOpenBRRule 0]                      [generalCloseBRRule]
-rule (Control Fin)    = GrammarRule (Control Fin)    [flipIsRequired (setArgSets generalOpenBRRule 3)]     [(flipIsRequired (setArgSets generalCloseBRRule 0))]
+rule (Keyword Fish)   = GrammarRule (Keyword Fish)   True  [generalOpenBRRule]                                   [generalCloseBRRule]
+rule (Keyword Route)  = GrammarRule (Keyword Route)  True  [generalOpenBRRule]                                   [generalCloseBRRule]
+rule (Keyword School) = GrammarRule (Keyword School) True  [flipIsRequired generalOpenBRRule, generalOpenBRRule] [generalCloseBRRule]
+rule (Keyword Shoal)  = GrammarRule (Keyword Shoal)  True  [setArgSets generalOpenBRRule 0]                      [generalCloseBRRule]
+rule (Control Fin)    = GrammarRule (Control Fin)    False [flipIsRequired (setArgSets generalOpenBRRule 3)]     [(flipIsRequired (setArgSets generalCloseBRRule 0))]
 rule _ = NoGR
+
+
+grammarRuleRequiredGroupLength :: GrammarRule -> Int
+grammarRuleRequiredGroupLength gr = 1 + (length (sendRules gr)) + (length (returnRules gr))
+
+
+isValidGroupBracketGrammar :: BracketRule -> [Token] -> Bool
+isValidGroupBracketGrammar br ts
+    | not isCompleteNestedCollapsible (ncCase br) ts = False
+    | argsRequired br && (argSets br) == 0           = null (getNestedCollapsibleContents (ncCase br))
+    | argsRequired br                                = completeArgumentSet (getNestedCollapsibleContents (ncCase br) ts) ((argSets br) - 1)
+    | otherwise                                      = True
+    where 
+        completeArgumentSet :: [Token] -> Int -> Bool
+        completeArgumentSet ts 0 = not (null ts) && null second --Too many arguments if False
+        completeArgumentSet ts commasRemaining
+            | null ts     = False --Not enough arguments
+            | null first  = False
+            | otherwise = completeArgumentSet (tail second) (commasRemaining - 1)
+            where 
+                break' = break ((Data (Punct ",")) ==) ts
+                first  = head break'
+                second = tail break'
+
+isValidGroupedTokenGrammar :: GrammarRule -> [[Token]] -> Bool
+isValidGroupedTokenGrammar gr tts
+    | grammarRuleRequiredGroupLength gr /= length tts             = False
+    | length (head tts) /= (1 + (if idRequired gr then 1 else 0)) = False
+    | 
