ee204fc77bd8f311782cc445f980ed88b0abe8cb
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-18 11:18:06 -0500

    Wrote some basic constructors and a 'throw' function

--- app/Exception.hs
+++ app/Exception.hs
@@ -47,7 +47,7 @@ constructExceptionSeverityFromLevel _ = ExceptionSeverity "Log"      1
 
 
 data ExceptionInfo = NoInfo | ExceptionInfo {
-    line :: Int,
+    --line :: Int,
     exceptionMessage :: String,
     severity :: ExceptionSeverity
 } deriving (Show, Eq)
@@ -63,3 +63,32 @@ instance Ord Exception where
     compare x y
         | exceptionType x /= exceptionType y = compare (exceptionType x) (exceptionType y)
         | otherwise                          = compare (severity (information x)) (severity (information y))
+
+
+constructException :: ExceptionType -> Exception
+constructException InvalidBracketing = Exception {
+    exceptionType = InvalidBracketing,
+    information = ExceptionInfo {
+        exceptionMessage = "Invalid Bracketing",
+        severity = constructExceptionSeverityFromLevel 4
+    }
+}
+constructException InvalidID = Exception {
+    exceptionType = InvalidID,
+    information = ExceptionInfo {
+        exceptionMessage = "Invalid ID for function",
+        severity = constructExceptionSeverityFromLevel 4
+    }
+}
+constructException InvalidArgs = Exception {
+    exceptionType = InvalidArgs,
+    information = ExceptionInfo {
+        exceptionMessage = "Invalid number of arguments in brackets",
+        severity = constructExceptionSeverityFromLevel 4
+    }
+}
+
+throw :: Exception -> IO ()
+throw e
+    | (severityLevel (severity (information e))) == 4 = die (show e)
+    | otherwise                                       = putStrLn (show e) --Include logging or something later idk
