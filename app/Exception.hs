36bee54c13d41f0efb0476913d03185fbb6a7023
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-18 10:43:47 -0500

    Some basic exception types and an instance of Ord

--- app/Exception.hs
+++ app/Exception.hs
@@ -2,3 +2,19 @@ module Exception (
 
 ) where
 
+import System.Exit
+import Data.List (findIndex)
+import Data.Ord (Ordering)
+
+data ExceptionType = General | InvalidBracketing | InvalidID | InvalidArgs deriving (Show,Read,Eq)
+
+exceptionTypeOrder :: [[ExceptionType]]
+exceptionTypeOrder = [[General], [InvalidBracketing, InvalidID, InvalidArgs]]
+
+instance Ord ExceptionType where
+    compare x y
+        | findIndex' x > findIndex' y = LT
+        | findIndex' x < findIndex' y = GT
+        | otherwise                   = EQ
+        where
+            findIndex' x' = findIndex (\e -> elem x' e) exceptionTypeOrder
