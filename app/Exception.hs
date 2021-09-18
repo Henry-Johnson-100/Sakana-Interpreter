9330fd089aff5f54f904caf2878ca91b6d9d60d2
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-18 11:23:25 -0500

    improved the ExceptionSeverity data structure and renamed the Exception constructor function to just 'exception'

--- app/Exception.hs
+++ app/Exception.hs
@@ -20,30 +20,7 @@ instance Ord ExceptionType where
             findIndex' x' = findIndex (\e -> elem x' e) exceptionTypeOrder
 
 
-data ExceptionSeverity = ExceptionSeverity {
-    severityLevelName :: String,
-    severityLevel     :: Int
-} deriving (Show,Eq,Ord)
-
-
-instance Bounded ExceptionSeverity where
-    minBound = ExceptionSeverity "Log" 1
-    maxBound = ExceptionSeverity "Fatal" 4
-
-
-constructExceptionSeverityFromName :: String -> ExceptionSeverity
-constructExceptionSeverityFromName "Fatal"    = ExceptionSeverity "Fatal"    4
-constructExceptionSeverityFromName "NonFatal" = ExceptionSeverity "NonFatal" 3
-constructExceptionSeverityFromName "Debug"    = ExceptionSeverity "Debug"    2
-constructExceptionSeverityFromName "Log"      = ExceptionSeverity "Log"      1
-constructExceptionSeverityFromName s          = ExceptionSeverity s          3
-
-
-constructExceptionSeverityFromLevel :: Int -> ExceptionSeverity
-constructExceptionSeverityFromLevel 4 = ExceptionSeverity "Fatal"    4
-constructExceptionSeverityFromLevel 3 = ExceptionSeverity "NonFatal" 3
-constructExceptionSeverityFromLevel 2 = ExceptionSeverity "Debug"    2
-constructExceptionSeverityFromLevel _ = ExceptionSeverity "Log"      1
+data ExceptionSeverity = Fatal | NonFatal | Debug | Log deriving (Show,Eq,Ord)
 
 
 data ExceptionInfo = NoInfo | ExceptionInfo {
@@ -65,30 +42,30 @@ instance Ord Exception where
         | otherwise                          = compare (severity (information x)) (severity (information y))
 
 
-constructException :: ExceptionType -> Exception
-constructException InvalidBracketing = Exception {
+exception :: ExceptionType -> Exception
+exception InvalidBracketing = Exception {
     exceptionType = InvalidBracketing,
     information = ExceptionInfo {
         exceptionMessage = "Invalid Bracketing",
-        severity = constructExceptionSeverityFromLevel 4
+        severity = Fatal
     }
 }
-constructException InvalidID = Exception {
+exception InvalidID = Exception {
     exceptionType = InvalidID,
     information = ExceptionInfo {
         exceptionMessage = "Invalid ID for function",
-        severity = constructExceptionSeverityFromLevel 4
+        severity = Fatal
     }
 }
-constructException InvalidArgs = Exception {
+exception InvalidArgs = Exception {
     exceptionType = InvalidArgs,
     information = ExceptionInfo {
         exceptionMessage = "Invalid number of arguments in brackets",
-        severity = constructExceptionSeverityFromLevel 4
+        severity = Fatal
     }
 }
 
 throw :: Exception -> IO ()
 throw e
-    | (severityLevel (severity (information e))) == 4 = die (show e)
+    | (severity (information e)) == Fatal = die (show e)
     | otherwise                                       = putStrLn (show e) --Include logging or something later idk
