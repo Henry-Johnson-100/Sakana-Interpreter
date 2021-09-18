485181bdb39aadf74b3d587d2e0c1c03e655c4c9
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-18 11:05:34 -0500

    Exception data type and an Ord instance as well as ExceptionSeverity data type and constructors

--- app/Exception.hs
+++ app/Exception.hs
@@ -6,7 +6,7 @@ import System.Exit
 import Data.List (findIndex)
 import Data.Ord (Ordering)
 
-data ExceptionType = General | InvalidBracketing | InvalidID | InvalidArgs deriving (Show,Read,Eq)
+data ExceptionType = General | InvalidBracketing | InvalidID | InvalidArgs deriving (Show,Eq)
 
 exceptionTypeOrder :: [[ExceptionType]]
 exceptionTypeOrder = [[General], [InvalidBracketing, InvalidID, InvalidArgs]]
@@ -18,3 +18,48 @@ instance Ord ExceptionType where
         | otherwise                   = EQ
         where
             findIndex' x' = findIndex (\e -> elem x' e) exceptionTypeOrder
+
+
+data ExceptionSeverity = ExceptionSeverity {
+    severityLevelName :: String,
+    severityLevel     :: Int
+} deriving (Show,Eq,Ord)
+
+
+instance Bounded ExceptionSeverity where
+    minBound = ExceptionSeverity "Log" 1
+    maxBound = ExceptionSeverity "Fatal" 4
+
+
+constructExceptionSeverityFromName :: String -> ExceptionSeverity
+constructExceptionSeverityFromName "Fatal"    = ExceptionSeverity "Fatal"    4
+constructExceptionSeverityFromName "NonFatal" = ExceptionSeverity "NonFatal" 3
+constructExceptionSeverityFromName "Debug"    = ExceptionSeverity "Debug"    2
+constructExceptionSeverityFromName "Log"      = ExceptionSeverity "Log"      1
+constructExceptionSeverityFromName s          = ExceptionSeverity s          3
+
+
+constructExceptionSeverityFromLevel :: Int -> ExceptionSeverity
+constructExceptionSeverityFromLevel 4 = ExceptionSeverity "Fatal"    4
+constructExceptionSeverityFromLevel 3 = ExceptionSeverity "NonFatal" 3
+constructExceptionSeverityFromLevel 2 = ExceptionSeverity "Debug"    2
+constructExceptionSeverityFromLevel _ = ExceptionSeverity "Log"      1
+
+
+data ExceptionInfo = NoInfo | ExceptionInfo {
+    line :: Int,
+    exceptionMessage :: String,
+    severity :: ExceptionSeverity
+} deriving (Show, Eq)
+
+
+data Exception = Exception {
+    exceptionType :: ExceptionType,
+    information   :: ExceptionInfo
+} deriving(Show, Eq)
+
+
+instance Ord Exception where
+    compare x y
+        | exceptionType x /= exceptionType y = compare (exceptionType x) (exceptionType y)
+        | otherwise                          = compare (severity (information x)) (severity (information y))
