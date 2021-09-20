83abc75688c063e068f9f8f68a0ecca61baf3b59
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-20 15:58:52 -0500

    Minor changes to other files for debugging and testing

--- app/PTIO.hs
+++ app/PTIO.hs
@@ -2,11 +2,11 @@ import System.IO
 import System.Environment
 import ParseTree
 import Lexer
-import Token.Keyword
+import Token.Data
 
 printTokens xs = concat $ map (show) $ tokenize xs
 
-printTree c = fPrintTree 0 (generateParseTree (tokenize c) (ParseTree (PacketUnit (Keyword Shoal) 0) []))
+printTree c = fPrintTree 0 (generateParseTree (tokenize c) (ParseTree (PacketUnit ((Data (Id "Main"))) 0) []))
 
 main = do
     args <- getArgs
