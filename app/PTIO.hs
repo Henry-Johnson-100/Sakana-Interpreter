5222c2408e0ce6eccd66d9072015a7d22e733f31
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-21 15:54:58 -0500

    Commented these files out after merge to use/ restore later

--- app/PTIO.hs
+++ app/PTIO.hs
@@ -1,16 +1,16 @@
-import System.IO
-import System.Environment
-import ParseTree
-import Lexer
-import Token.Data
+-- import System.IO
+-- import System.Environment
+-- import ParseTree
+-- import Lexer
+-- import Token.Data
 
-printTokens xs = concatMap show (tokenize xs)
+-- printTokens xs = concatMap show (tokenize xs)
 
-printTree c = fPrintTree 0 (generateParseTree (tokenize c) (ParseTree (PacketUnit (Data (Id "Main")) 0) []))
+-- printTree c = fPrintTree 0 (generateParseTree (tokenize c) (ParseTree (PacketUnit (Data (Id "Main")) 0) []))
 
-main = do
-    args <- getArgs
-    handle <- openFile (head args) ReadMode
-    contents <- hGetContents handle
-    putStr $ printTree contents
-    hClose handle
+-- main = do
+--     args <- getArgs
+--     handle <- openFile (head args) ReadMode
+--     contents <- hGetContents handle
+--     putStr $ printTree contents
+--     hClose handle
