d4eff385f70d7f81677ec50193fa4d953b9c9ae3
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-09-28 14:42:17 -0500

    Removed Headless trees, made it so that all the tree generating functions would instead generate a list of trees then concat them all together in the end, even if a single tree is only one member long

--- app/Token/Util/Tree.hs
+++ app/Token/Util/Tree.hs
@@ -4,20 +4,15 @@ class TreeIO r where
   fPrintTree :: (Show a) => Int -> r a -> String
   ioPrintTree :: (Show a) => r a -> IO ()
 
-data Tree a = Empty | a :-<-: [Tree a] | Headless [Tree a] deriving (Show, Eq)
+data Tree a = Empty | a :-<-: [Tree a] deriving (Show, Eq)
 
 instance TreeIO Tree where
   fPrintTree d Empty = concat (replicate (d * 4 - 1) "-") ++ ">" ++ "Empty\n"
-  fPrintTree d (Headless a) = concat (replicate (d * 4 - 1) "-") ++ ">" ++ "HEADLESS\n" ++ concatMap (fPrintTree (d + 1)) a
   fPrintTree d (n :-<-: a) = concat (replicate (d * 4 - 1) "-") ++ ">" ++ show n ++ "\n" ++ concatMap (fPrintTree (d + 1)) a
   ioPrintTree t = putStrLn $ fPrintTree 0 t
 
 instance Functor Tree where
   fmap f (b :-<-: cs) = f b :-<-: map (fmap f) cs
-
-isHeadless :: Tree a -> Bool
-isHeadless (Headless _) = True
-isHeadless _ = False
 
 tree :: a -> Tree a
 tree x = x :-<-: []
@@ -31,23 +26,19 @@ serialTree [x] = tree x
 serialTree (x : xs) = tree x -<- serialTree xs
 
 treeNode :: Tree a -> a
-treeNode (b :-<-: _) = b
+treeNode (n :-<-: _) = n
 
 treeChildren :: Tree a -> [Tree a]
 treeChildren (_ :-<-: cs) = cs
-treeChildren (Headless cs) = cs
 
 transplantChildren :: Tree a -> Tree a -> Tree a
 transplantChildren t (_ :-<-: cs) = t -<= cs
 
 (-<-) :: Tree a -> Tree a -> Tree a
-t -<- (Headless cs) = t -<= cs
 (b :-<-: cs) -<- t = b :-<-: (cs ++ [t])
-(Headless cs) -<- t = Headless (cs ++ [t])
 
 (-<=) :: Tree a -> [Tree a] -> Tree a
 (b :-<-: cs) -<= ts = b :-<-: (cs ++ ts)
-(Headless cs) -<= ts = Headless (cs ++ ts)
 
 lookupOn :: Tree a -> (Tree a -> Bool) -> [Tree a]
 lookupOn Empty _ = [Empty]
