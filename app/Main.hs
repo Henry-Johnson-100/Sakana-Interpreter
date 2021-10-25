efd57ebe28ece1fccc712b28073909033d72a37c
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-10-25 09:26:03 -0500

    Added some simple -v and -h arguments to the main Sakana executable
    # Conflicts:
    #	app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
-import TreeInterpreter (executeMain, getMainEnvironmentStack, getMainExecutionTrees)
+import Data.Version (showVersion)
 import Lexer (tokenize)
+import Paths_Sakana (version)
 import SyntaxTree (generateSyntaxTree)
 import System.Environment (getArgs)
 import System.IO
@@ -13,16 +14,39 @@ import System.IO
     stdout,
   )
 import qualified Token.Data as D (fromData)
+import TreeInterpreter (executeMain, getMainEnvironmentStack, getMainExecutionTrees)
 
 main :: IO ()
 main = do
   args <- getArgs
-  handle <- openFile (head args) ReadMode
-  contents <- hGetContents handle
-  fileTree <- return . generateSyntaxTree . tokenize $ contents
-  result <-
-    executeMain
-      ((return . getMainEnvironmentStack) fileTree)
-      ((return . getMainExecutionTrees) fileTree)
-  (hPutStrLn stdout . D.fromData) result
-  hClose handle
+  usingArgs args
+
+usingArgs :: [String] -> IO ()
+usingArgs args
+  | argsContainAny ["-h", "--help"] =
+    putStrLn
+      "Usage: Sakana [path] [-v, --version] [-h, --help]\n\
+      \\tPath:\n\
+      \\t\tPath to the file to interpret.\n\
+      \\tFlags:\n\
+      \\t-v, --version\n\
+      \\t\tDisplays the version of the Sakana Interpreter.\n\
+      \\t-h, --help\n\
+      \\t\tDisplays this help."
+  | argsContainAny ["-v", "--version"] =
+    putStrLn $ "Sakana Interpeter Version: " ++ (showVersion Paths_Sakana.version)
+  | otherwise = interpretFileAndReturn (head args)
+  where
+    argsContainAny = any (`elem` args)
+
+interpretFileAndReturn :: FilePath -> IO ()
+interpretFileAndReturn filePathToInterpret = do
+  fileHandle <- openFile filePathToInterpret ReadMode
+  fileTree <-
+    hGetContents fileHandle
+      >>= return . generateSyntaxTree . tokenize
+  executeMain
+    ((return . getMainEnvironmentStack) fileTree)
+    ((return . getMainExecutionTrees) fileTree)
+    >>= hPutStrLn stdout . D.fromData
+  hClose fileHandle
