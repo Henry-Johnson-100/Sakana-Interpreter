3aff61c07d5a20054b8a2fbc7773365205177977
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-10-26 10:03:44 -0500

    finished making primitives according to my grammar

--- app/SakanaParserCombinator.hs
+++ app/SakanaParserCombinator.hs
@@ -5,6 +5,7 @@ where
 
 -- import Text.Parsec.Prim as Parsec.Prim
 
+import Control.Applicative
 import qualified Data.List as DList
 import qualified Exception.Base as Exception
 import qualified Lexer
@@ -23,7 +24,6 @@ import qualified Util.General as Util
 import qualified Util.Like as Like (Like (..))
 import Util.Tree (Tree (..), (-<-), (-<=))
 import qualified Util.Tree as Tree
-import Control.Applicative
 
 newtype Parser a = Parser {parse :: [Lexer.TokenUnit] -> [(a, [Lexer.TokenUnit])]}
 
@@ -52,7 +52,9 @@ t' = Lexer.tokenize "fish hello >(x)> <(\"hello\")<"
 fstBifunctorMap :: (a -> c) -> [(a, b)] -> [(c, b)]
 fstBifunctorMap f tupAB = [(f a', b') | (a', b') <- tupAB]
 
--- Primitive tokens
+-- Primitive parsers----------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+
 keyword :: K.Keyword -> Parser Lexer.TokenUnit
 keyword k =
   Parser
@@ -71,11 +73,32 @@ isId =
           else []
     )
 
-isData :: p -> Parser Lexer.TokenUnit
-isData d = Parser (\(tu : tus) -> if isDataAndNotId tu then [(tu, tus)] else [])
+isData :: Parser Lexer.TokenUnit
+isData = Parser (\(tu : tus) -> if isDataAndNotId tu then [(tu, tus)] else [])
   where
     isDataAndNotId d' =
       Util.foldIdApplicativeOnSingleton
         all
         [Like.like Lexer.genericData, not . Lexer.dataTokenIsId]
         (Lexer.unit d')
+
+control :: C.Control -> Parser Lexer.TokenUnit
+control c =
+  Parser
+    ( \(tu : tus) ->
+        if (Lexer.unit tu) == (Lexer.Control c) then [(tu, tus)] else []
+    )
+
+bracket :: B.ScopeType -> B.BracketTerminal -> Parser Lexer.TokenUnit
+bracket sc bt =
+  Parser
+    ( \(tu : tus) ->
+        if (Lexer.unit tu) == (Lexer.Bracket sc bt) then [(tu, tus)] else []
+    )
+
+anyOp :: Parser Lexer.TokenUnit
+anyOp =
+  Parser
+    ( \(tu : tus) ->
+        if (Lexer.unit tu) `Like.like` Lexer.genericOperator then [(tu, tus)] else []
+    )
