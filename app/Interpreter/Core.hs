1e349d944ca98157700177ab386d21eac56de145
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-12-30 09:05:38 -0600

    Reintegrated Interpreter to Interpreter.Core becase the separation wasn't needed after all.

--- app/Interpreter/Core.hs
+++ app/Interpreter/Core.hs
@@ -1,5 +1,5 @@
 module Interpreter.Core
-  ( nodeIsStandardLibCall,
+  (
   )
 where
 
@@ -17,6 +17,45 @@ import Util.General ((.<))
 import qualified Util.General as UGen
 import qualified Util.Tree as Tree
 import Prelude hiding (lookup)
+
+evaluateProgram :: Env.Runtime -> IO Syntax.SyntaxTree
+evaluateProgram = fmap programOutputHead . interpret
+  where
+    programOutputHead :: Env.Runtime -> Syntax.SyntaxTree
+    programOutputHead =
+      Maybe.fromMaybe
+        (Exception.raiseError programTerminatesAsNull)
+        . (UGen.head' . Env.runtimeValue . Env.throwJustError)
+    programTerminatesAsNull :: Exception.Exception
+    programTerminatesAsNull =
+      Exception.newException
+        Exception.NullTree
+        []
+        "Null value at end of program."
+        Exception.Fatal
+
+interpret :: Env.Runtime -> IO Env.Runtime
+interpret rt = interpret' . Env.throwJustError $ rt
+  where
+    interpret' :: Env.Runtime -> IO Env.Runtime
+    -- Will cause an infinite hang if Env.throwJustError is not present in interpret.
+    interpret' (Env.Runtime st [] err) =
+      interpret
+        ( Env.replaceException
+            ( Exception.newException
+                Exception.NullTree
+                []
+                "Unexpected null tree in the interpreter."
+                Exception.Fatal
+            )
+            rt
+        )
+    -- Guards go here.
+    -- trs could be null.
+    interpret' (Env.Runtime st (tr : trs) err)
+      | Inspect.treeHeadIsPrimitiveData tr = return rt
+      | treeHeadIsStandardLibraryCall tr = return rt
+      | otherwise = return rt
 
 -- | #TODO
 evaluateFunction :: Env.Runtime -> IO Env.Runtime
@@ -47,6 +86,14 @@ evaluateStandardLibraryCall rt = evaluateStandardLibraryCall' rt
               . (=<<) (Syntax.baseData . Syntax.token)
               . Tree.treeNode
           )
+
+treeHeadIsStandardLibraryCall :: Syntax.SyntaxTree -> Bool
+treeHeadIsStandardLibraryCall =
+  UGen.foldIdApplicativeOnSingleton
+    all
+    [ Inspect.treeHeadIsFunctionCall,
+      Tree.nodeStrictlySatisfies Interpreter.Core.nodeIsStandardLibCall
+    ]
 
 nodeIsStandardLibCall :: Syntax.SyntaxUnit -> Bool
 nodeIsStandardLibCall su = case Syntax.token su of
