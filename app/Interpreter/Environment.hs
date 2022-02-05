794a3a450d4da4c227b7650840d06c141d21174b
Henry-Johnson-100 <hencutJohnson@gmail.com>
2022-02-05 08:25:21 -0600

    Substantially modified Runtime and Binding data types
    Added a new list field called bindingParams in Binding. This is to make it easier to examine a tree's parameters for partial application and symbol lookup
    
    Changed a Runtime's symbol table to a list of symbol tables to help with creating distinct closure environments in function calls

--- app/Interpreter/Environment.hs
+++ app/Interpreter/Environment.hs
@@ -7,21 +7,23 @@ module Interpreter.Environment
     Runtime (..),
     insertBinding,
     singletonSymbolTable,
+    newSymbolTable,
     lookup,
     maybeLookup,
     bindingExists,
-    replaceSymbolTable,
+    replaceSymbolTableStack,
+    pushSymbolTable,
+    popSymbolTable,
     replaceValue,
     replaceException,
     propagateException,
-    transpropagateUnion,
-    cisUnion,
     transUnion,
+    cisUnion,
+    cleanRuntimeStack,
     throwJustError,
     runtimeBindingExists,
     runtimeMaybeLookup,
     runtimeLookup,
-    updateRuntimeSymbolTable,
     injectBinding,
   )
 where
@@ -50,15 +52,21 @@ instance UC.Format BindingKey where
 
 data Binding = Binding
   { bindingKey :: !BindingKey,
+    bindingParams :: ![Syntax.SyntaxUnit],
     bindingTree :: !Syntax.SyntaxTree
   }
   deriving (Show, Eq)
 
 instance UC.Defaultable Binding where
-  defaultValue = Binding (BindingKey "") (UC.defaultValue)
+  defaultValue = Binding (BindingKey "") [] (UC.defaultValue)
 
 instance UC.Format Binding where
-  format (Binding bk tree) = (UC.format bk) ++ ": " ++ (UC.format tree)
+  format (Binding bk params tree) =
+    (UC.format bk)
+      ++ ": "
+      ++ (unwords . map UC.format) params
+      ++ " ->\n"
+      ++ (UC.format tree)
 
 type SymbolTable = HashMap.HashMap BindingKey Syntax.SyntaxTree
 
@@ -72,20 +80,20 @@ instance (UC.Format k, UC.Format v) => UC.Format (HashMap.HashMap k v) where
       formatKVTuple (k, v) = UC.format k ++ ": " ++ UC.format v
 
 data Runtime = Runtime
-  { runtimeSymbolTable :: !SymbolTable,
+  { runtimeSymbolTableStack :: ![SymbolTable],
     runtimeValue :: ![Syntax.SyntaxTree],
     runtimeException :: !(Maybe.Maybe Exception.Exception)
   }
   deriving (Eq, Show)
 
 instance UC.Defaultable Runtime where
-  defaultValue = Runtime HashMap.empty [] Maybe.Nothing
+  defaultValue = Runtime [] [] Maybe.Nothing
 
 instance UC.Format Runtime where
   format rt =
     unlines
       [ "SYMBOL TABLE",
-        (UC.format . runtimeSymbolTable) rt,
+        (UC.format . runtimeSymbolTableStack) rt,
         "VALUE",
         (UC.format . runtimeValue) rt
       ]
@@ -103,19 +111,38 @@ insertBinding st = flip (uncurry HashMap.insert) st . bindingTuple
 singletonSymbolTable :: Binding -> SymbolTable
 singletonSymbolTable = insertBinding HashMap.empty
 
+newSymbolTable :: [Binding] -> SymbolTable
+newSymbolTable [] = UC.defaultValue
+newSymbolTable (b : bs) = List.foldl' insertBinding (singletonSymbolTable b) bs
+
 lookup :: SymbolTable -> Syntax.SyntaxUnit -> Syntax.SyntaxTree
 lookup st (Syntax.SyntaxUnit (Syntax.Data (Syntax.Id id)) line _) =
   Maybe.fromMaybe
-    (Exception.raiseError symbolNotFoundException)
+    ((Exception.raiseError .< symbolNotFoundException) id line)
     (maybeLookup st id)
-  where
-    symbolNotFoundException =
-      Exception.newException
-        Exception.SymbolNotFound
-        [line]
-        ("Cannot find binding for ID: " ++ id)
-        Exception.Fatal
 lookup _ su = (Exception.raiseError . improperLookupException) su
+
+stackLookup :: [SymbolTable] -> Syntax.SyntaxUnit -> Syntax.SyntaxTree
+stackLookup sts (Syntax.SyntaxUnit (Syntax.Data (Syntax.Id id)) line _) =
+  Maybe.fromMaybe
+    ((Exception.raiseError .< symbolNotFoundException) id line)
+    (maybeStackLookup sts id)
+stackLookup _ su = (Exception.raiseError . improperLookupException) su
+
+maybeStackLookup :: [SymbolTable] -> String -> Maybe Syntax.SyntaxTree
+maybeStackLookup (st : sts) str =
+  case maybeLookup st str of
+    Maybe.Nothing -> maybeStackLookup sts str
+    result -> result
+maybeStackLookup [] _ = Maybe.Nothing
+
+symbolNotFoundException :: [Char] -> Int -> Exception.Exception
+symbolNotFoundException id line =
+  Exception.newException
+    Exception.SymbolNotFound
+    [line]
+    ("Cannot find binding for ID: " ++ id)
+    Exception.Fatal
 
 improperLookupException :: Syntax.SyntaxUnit -> Exception.Exception
 improperLookupException su =
@@ -131,13 +158,29 @@ bindingExists st su = case su of
     (Maybe.isJust . maybeLookup st) id
   _ -> (Exception.raiseError . improperLookupException) su
 
+stackBindingExists :: [SymbolTable] -> Syntax.SyntaxUnit -> Bool
+stackBindingExists [] _ = False
+stackBindingExists (st : sts) su =
+  if bindingExists st su then True else stackBindingExists sts su
+
 maybeLookup :: SymbolTable -> String -> Maybe.Maybe Syntax.SyntaxTree
 maybeLookup st str = HashMap.lookup (BindingKey str) (st)
 
+cleanStack :: [SymbolTable] -> [SymbolTable]
+cleanStack = filter (not . UC.isDefault)
+
 ----Runtime Operations--------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
-replaceSymbolTable :: SymbolTable -> Runtime -> Runtime
-replaceSymbolTable st rt = rt {runtimeSymbolTable = st}
+replaceSymbolTableStack :: [SymbolTable] -> Runtime -> Runtime
+replaceSymbolTableStack st rt = rt {runtimeSymbolTableStack = st}
+
+pushSymbolTable :: SymbolTable -> Runtime -> Runtime
+pushSymbolTable st (Runtime sts trs err) = Runtime (st : sts) trs err
+
+-- | Returns the current top frame or an empty frame if none are present.
+popSymbolTable :: Runtime -> SymbolTable
+popSymbolTable (Runtime [] _ _) = UC.defaultValue
+popSymbolTable (Runtime (st : sts) _ _) = st
 
 replaceValue :: [Syntax.SyntaxTree] -> Runtime -> Runtime
 replaceValue trs rt = rt {runtimeValue = trs}
@@ -148,65 +191,42 @@ replaceException ex rt = rt {runtimeException = ex}
 propagateException :: Runtime -> Runtime -> Runtime
 propagateException rtex rt = replaceException (runtimeException rtex) rt
 
-{-
-On unioning Runtime SymbolTables:
-  4 permutations of Runtime unions where R is a runtime and T is a SymbolTable ->
-    where Ta U Tb -> Tf (Tfst)
-          Tb U Ta -> Ts (Tsnd)
-
-            cis-flip(|)    transpropagation(/)
-    Ra   Rb     Ra         Rb
-       U    ->  |     X    |
-    Ta   Tb     Tf         Ts
-                           trans/cis-union product
-For (R T)a<|>b as a or b:
-So, cisUnion   a b -> Rb Ts
-               b a -> Ra Tf (cis-flip)
-
-    transUnion a b -> Ra Ts
-               b a -> Rb Tf (trans-propagation)
--}
-
--- | Transpropagate a union of two Runtimes' SymbolTables, with the first as the master,
--- to the second Runtime.
+-- | Push the first runtime's stack to the head of the second.
 --
--- defined as:
--- >flip transUnion
-transpropagateUnion :: Runtime -> Runtime -> Runtime
-transpropagateUnion = flip transUnion
-
--- | Replace a union of two Runtimes' SymbolTables, with the second as the master,
--- into the second Runtime.
-cisUnion :: Runtime -> Runtime -> Runtime
-cisUnion ra rb =
-  let newTable = HashMap.union (runtimeSymbolTable rb) (runtimeSymbolTable ra)
-   in replaceSymbolTable newTable rb
-
--- | Replace a union of two Runtimes' SymbolTables, with the second as the master,
--- into the first Runtime.
+-- Keep the second's value and exception.
 transUnion :: Runtime -> Runtime -> Runtime
-transUnion ra rb =
-  let newTable = HashMap.union (runtimeSymbolTable rb) (runtimeSymbolTable ra)
-   in replaceSymbolTable newTable ra
+transUnion (Runtime stsa _ _) (Runtime stsb trsb errb) =
+  Runtime (stsa ++ stsb) trsb errb
+
+-- | Push the first runtime's stack to the head of the second.
+--
+-- Kepp the first's value and exception.
+cisUnion :: Runtime -> Runtime -> Runtime
+cisUnion (Runtime stsa trsa erra) (Runtime stsb _ _) =
+  Runtime (stsa ++ stsb) trsa erra
+
+-- | Removes empty stack frames from a Runtime.
+cleanRuntimeStack :: Runtime -> Runtime
+cleanRuntimeStack (Runtime sts trs err) = Runtime (cleanStack sts) trs err
 
 throwJustError :: Runtime -> Runtime
 throwJustError rt = Maybe.maybe rt Exception.raiseError (runtimeException rt)
 
 runtimeBindingExists :: Runtime -> Syntax.SyntaxUnit -> Bool
-runtimeBindingExists = bindingExists . runtimeSymbolTable
+runtimeBindingExists = stackBindingExists . runtimeSymbolTableStack
 
 runtimeLookup :: Runtime -> Syntax.SyntaxUnit -> Syntax.SyntaxTree
-runtimeLookup = lookup . runtimeSymbolTable
+runtimeLookup = stackLookup . runtimeSymbolTableStack
 
 runtimeMaybeLookup :: Runtime -> String -> Maybe Syntax.SyntaxTree
-runtimeMaybeLookup = maybeLookup . runtimeSymbolTable
+runtimeMaybeLookup = maybeStackLookup . runtimeSymbolTableStack
 
--- | Union a SymbolTable with a Runtime's SymbolTable, with the former as the master,
--- and replace it into the Runtime.
-updateRuntimeSymbolTable :: SymbolTable -> Runtime -> Runtime
-updateRuntimeSymbolTable st rt =
-  let newTable = HashMap.union st (runtimeSymbolTable rt)
-   in replaceSymbolTable newTable rt
+-- injectBinding :: Binding -> Runtime -> Runtime
+-- injectBinding = updateRuntimeSymbolTable . singletonSymbolTable
 
+-- | Inject a binding into the top frame of the symbol table stack
+-- Or create a new frame if no stack is present.
 injectBinding :: Binding -> Runtime -> Runtime
-injectBinding = updateRuntimeSymbolTable . singletonSymbolTable
+injectBinding b (Runtime [] trs err) = Runtime [(singletonSymbolTable b)] trs err
+injectBinding b (Runtime (st : sts) trs err) =
+  Runtime ((flip (:) sts . insertBinding st) b) trs err
